e1ab5148cf3ad3f7c6f53e6a77b828cbabbeef15
Nux007 <007.nux@gmail.com>
2021-02-24 07:04:53 +0100

    Exo 193.

---
+++ Classes/DB.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Handle MySQL Connection with PDO.
+ * Class DB
+ */
+class DB
+{
+    private string $server = 'localhost';
+    private string $db = 'live';
+    private string $user = 'root';
+    private string $pwd = '';
+
+    private static PDO $dbInstance;
+
+    /**
+     * DbStatic constructor.
+     */
+    public function __construct() {
+        try {
+            self::$dbInstance = new PDO("mysql:host=$this->server;dbname=$this->db;charset=utf8", $this->user, $this->pwd);
+            self::$dbInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $exception) {
+            echo $exception->getMessage();
+        }
+    }
+
+    /**
+     * Return PDO instance.
+     */
+    public static function getInstance(): ?PDO {
+        if( is_null(self::$dbInstance) ) {
+            new self();
+        }
+        return self::$dbInstance;
+    }
+
+    /**
+     * Avoid instance to be cloned.
+     */
+    public function __clone() {}
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * 1. Importez la table user dans une base de données que vous aurez créée au préalable via PhpMyAdmin
+ * 2. En utilisant l'objet de connexion qui a déjà été défini =>
+ *    --> Remplacez les informations de connexion ( nom de la base et vérifiez les paramètres d'accès ).
+ *    --> Supprimez le dernier utilisateur de la liste, faites une capture d'écran dans PhpMyAdmin pour me montrer que vous avez supprimé l'entrée et pushez la avec votre code.
+ *    --> Faites un truncate de la base de données, les auto incréments présents seront remis à 0
+ *    --> Insérez un nouvel utilisateur dans la table ( faites un screenshot et ajoutez le au repo )
+ *    --> Finalement, vous décidez de supprimer complètement la table
+ *    --> Et pour finir, comme vous n'avez plus de table dans la base de données, vous décidez de supprimer aussi la base de données.
+ */
